bb962018d3b4d053ba608b3948f551cb39afc838
Geert Gerritsen <444568+ggerritsen@users.noreply.github.com>
2023-12-01 22:41:05 +0100

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,17 @@
 
 fn main() {
-    let test_input: &str =
-        "1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet";
+    let digits = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let input_test: &str =
+        "two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen";
 
     let input: &str = include_str!("input.txt");
 
@@ -14,12 +21,30 @@ fn main() {
         println!("Processing line: {}", line);
 
         // get first digit
-        let index_first = line.find(char::is_numeric).unwrap();
-        let first = line.chars().nth(index_first).unwrap();
+        let mut index_first = line.find(char::is_numeric).or(Some(999999999)).unwrap();
+        let mut first = line.chars().nth(index_first).or(Some('0')).unwrap().to_string();
+        for count in 0..9 {
+            let digit = digits[count];
+            let index = line.find(digit).or(Some(999999999)).unwrap();
+            if index < index_first {
+                index_first = index;
+                first = (count + 1).to_string();
+            }
+        }
+        println!("First digit is: {}", first);
 
         // get last digit
-        let index_last = line.rfind(char::is_numeric).unwrap();
-        let last = line.chars().nth(index_last).unwrap();
+        let mut index_last = line.rfind(char::is_numeric).or(Some(0)).unwrap();
+        let mut last = line.chars().nth(index_last).or(Some('0')).unwrap().to_string();
+        for count in 0..9 {
+            let digit = digits[count];
+            let index = line.rfind(digit).or(Some(0)).unwrap();
+            if index > index_last {
+                index_last = index;
+                last = (count + 1).to_string();
+            }
+        }
+        println!("Last digit is: {}", first);
 
         // combine them into a number
         let number = format!("{first}{last}");
