89e75c1e2d9f59e0dc042092e54725479106f27f
Geert Gerritsen <444568+ggerritsen@users.noreply.github.com>
2023-12-04 21:13:38 +0100

    Fix some clippy warnings

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,4 @@
-use std::cmp::{max, min};
-
 fn main() {
-
     let input: &str = include_str!("input.txt");
 
     let _input: &str = "467..114..
@@ -19,10 +16,18 @@ fn main() {
 
     let has_nearby_symbol = |x, y, number_length: usize| -> bool {
         let min_x = if x > 0 { x - 1 } else { 0 };
-        let max_x = if x < grid.len()-1 { x + 1 } else { grid.len()-1};
+        let max_x = if x < grid.len() - 1 {
+            x + 1
+        } else {
+            grid.len() - 1
+        };
 
         let row: &Vec<char> = &grid[x];
-        let min_y = if y - number_length > 0 { y - number_length - 1 } else { 0 };
+        let min_y = if y - number_length > 0 {
+            y - number_length - 1
+        } else {
+            0
+        };
         let max_y = if y < row.len() { y + 1 } else { row.len() };
 
         // println!("Checking for nearby symbol between {},{} and {},{}", min_x, min_y, max_x, max_y);
@@ -47,11 +52,11 @@ fn main() {
     // * check if there's a symbol in the next row (y+1) (x-1, x+1)
     let mut sum = 0;
     for i in 0..grid.len() {
-        let mut number = "".to_string();
+        let mut number = String::new();
         for j in 0..grid[i].len() {
-            let cell  = grid[i][j];
+            let cell = grid[i][j];
             if cell.is_ascii_digit() {
-                println!("Found number {} at location {},{}", cell, i, j);
+                println!("Found number {cell} at location {i},{j}");
                 number.push(cell);
 
                 // if we're at the end of a row, then the number is full
@@ -60,52 +65,46 @@ fn main() {
                     // println!("Found full number {} at location {},{}", full_number, i, j);
 
                     if has_nearby_symbol(i, j, number.len()) {
-                        println!("Found part number {} at location {},{}", full_number, i, j);
+                        println!("Found part number {full_number} at location {i},{j}");
                         sum += full_number;
                     }
                     number.clear();
                 }
 
                 continue;
-            } else {
-                if number.len() > 0 {
-                    let full_number = number.parse::<i32>().unwrap();
-                    // println!("Found full number {} at location {},{}", full_number, i, j);
-
-                    if has_nearby_symbol(i, j, number.len()) {
-                        println!("Found part number {} at location {},{}", full_number, i, j);
-                        sum += full_number;
-                    }
-                    number.clear();
-                }
             }
+            if !number.is_empty() {
+                let full_number = number.parse::<i32>().unwrap();
+                // println!("Found full number {} at location {},{}", full_number, i, j);
 
+                if has_nearby_symbol(i, j, number.len()) {
+                    println!("Found part number {full_number} at location {i},{j}");
+                    sum += full_number;
+                }
+                number.clear();
+            }
         }
     }
 
-    println!("Sum is {}", sum);
+    println!("Sum is {sum}");
 
     let find_two_nearby_parts = |x, y: usize| -> Option<i32> {
-        
         return Some(0);
     };
 
     let mut product_sum = 0;
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
-            let cell  = grid[i][j];
+            let cell = grid[i][j];
             if cell == '*' {
-                // println!("Found gear at location {},{}", i, j);
-
                 let product = find_two_nearby_parts(i, j);
                 if product.is_some() {
-                    println!("Found gear at location {},{}", i, j);
+                    println!("Found gear at location {i},{j}");
                     product_sum += product.unwrap();
                 }
             }
         }
     }
 
-    println!("Product sum is {}", product_sum);
-
+    println!("Product sum is {product_sum}");
 }
