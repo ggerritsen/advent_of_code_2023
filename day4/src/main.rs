908216b3742fffb16b0389ff459e7e52520dfac7
Geert Gerritsen <444568+ggerritsen@users.noreply.github.com>
2023-12-06 22:45:14 +0100

    Day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("input.txt");
 
@@ -9,47 +11,84 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
     let mut cards: Vec<Card> = Vec::new();
-    for line in input.lines(){
-        let mut parts = line.split(":");
-        let title = parts.nth(0).unwrap();
+    for line in input.lines() {
+        let mut parts = line.split(':');
+        // let title = parts.nth(0).unwrap();
+        // title.r
+        // println!("parsing {line}");
+        // println!("parsing now {:?}", parts.nth(0).unwrap().split(" ").filter(|x| !x.is_empty()).nth(1));
+        let index: i32 = parts
+            .next()
+            .unwrap()
+            .split(' ')
+            .filter(|x| !x.is_empty())
+            .nth(1)
+            .unwrap()
+            .trim()
+            .parse()
+            .unwrap();
 
         let mut inner_parts = parts.nth(0).unwrap().split("|");
         let mut winning_numbers: Vec<i32> = Vec::new();
         for i in inner_parts.nth(0).unwrap().split(" ") {
             if i.trim() == "" {
-                continue
+                continue;
             }
-            let n:i32 = i.trim().parse().unwrap();
+            let n: i32 = i.trim().parse().unwrap();
             winning_numbers.push(n);
         }
         let mut my_numbers: Vec<i32> = Vec::new();
         for i in inner_parts.nth(0).unwrap().split(" ") {
             if i.trim() == "" {
-                continue
+                continue;
             }
             let n: i32 = i.trim().parse().unwrap();
             my_numbers.push(n);
         }
-        cards.push(Card {title, winning_numbers, my_numbers});
+        cards.push(Card {
+            index,
+            winning_numbers,
+            my_numbers,
+        });
     }
 
     let mut sum = 0;
-    for c in cards {
+    for c in &cards {
         sum += c.calculate_points();
     }
 
     println!("sum is {sum}");
+
+    let mut amounts = HashMap::new();
+    for c in &cards {
+        amounts.insert(c.index, 1);
+    }
+
+    for card in &cards {
+        for _ in 0..*amounts.get(&card.index).unwrap() {
+            let matches = card.calculate_matching_numbers();
+            // println!("card {} has {} matches", card.index, matches);
+            for i in card.index + 1..card.index + 1 + matches {
+                amounts.entry(i).and_modify(|amount| *amount += 1);
+            }
+        }
+    }
+
+    println!("total: {}", amounts.values().sum::<i32>());
 }
 
 struct Card {
-    title: &'static str,
+    index: i32,
     winning_numbers: Vec<i32>,
-    my_numbers: Vec<i32>
+    my_numbers: Vec<i32>,
 }
 
 impl Card {
     fn calculate_points(&self) -> i32 {
-        println!("calculating points for winning_nums {:?} with my_nums {:?}", &self.winning_numbers, &self.my_numbers);
+        println!(
+            "calculating points for winning_nums {:?} with my_nums {:?}",
+            &self.winning_numbers, &self.my_numbers
+        );
         let mut count = 0;
         for n in &self.my_numbers {
             if self.winning_numbers.contains(&n) {
@@ -57,10 +96,24 @@ impl Card {
             }
         }
         if count == 0 {
-            return 0
+            return 0;
         }
 
         let two: i32 = 2;
-        two.pow(count-1)
+        two.pow(count - 1)
+    }
+
+    fn calculate_matching_numbers(&self) -> i32 {
+        // println!(
+        //     "calculating points for winning_nums {:?} with my_nums {:?}",
+        //     &self.winning_numbers, &self.my_numbers
+        // );
+        let mut count = 0;
+        for n in &self.my_numbers {
+            if self.winning_numbers.contains(&n) {
+                count += 1;
+            }
+        }
+        count
     }
 }
