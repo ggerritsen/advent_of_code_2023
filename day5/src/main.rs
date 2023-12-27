b569702c59bd77d6ca8a7c18b8bd15fa0bd53720
Geert Gerritsen <444568+ggerritsen@users.noreply.github.com>
2023-12-27 17:38:28 +0100

    Use Iterators to reduce memory pressure

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,11 +3,19 @@ use std::time::Instant;
 fn main() {
     let input = include_str!("input.txt");
 
-    let mut seeds: Vec<i64> = Vec::new();
+    let mut seeds = Seeds::new(
+        input
+            .lines()
+            .next()
+            .unwrap()
+            .to_string()
+            .replace("seeds: ", ""),
+    );
+
     let mut filters: Vec<Filter> = Vec::new();
     let mut filter: Option<Filter> = None;
     for line in input.lines() {
-        // process first line
+        // skip first line, already processed
         if line.starts_with("seeds: ") {
             // part 1
             // seeds = line
@@ -19,8 +27,6 @@ fn main() {
             //     .collect();
 
             // part 2
-            seeds = parse_seeds(line.replace("seeds: ", ""));
-
             continue;
         }
 
@@ -34,7 +40,7 @@ fn main() {
 
         if line.ends_with("map:") {
             filter = Some(Filter {
-                name: line.to_string(),
+                _name: line.to_string(),
                 computations: Vec::new(),
             });
             // could parse the name but perhaps not needed
@@ -59,15 +65,17 @@ fn main() {
     }
 
     // println!("seeds is {:?}", seeds);
-    println!("len seeds is {:?}", seeds.len());
+    // println!("len seeds is {:?}", seeds.len());
     println!("len filters is {:?}", filters.len());
 
     // let mut results: Vec<i64> = Vec::new();
     let mut min_result = i64::MAX;
-    let mut now = Instant::now();
     let start = Instant::now();
-    for (i, s) in seeds.iter().enumerate() {
-        let mut result = *s;
+    let mut index: i64 = 0;
+    while let Some(item) = seeds.next() {
+        // println!("item is {:?}", item);
+
+        let mut result = item;
         for f in &filters {
             result = f.apply(result);
             // println!("filter {} applied, result is now {}", f.name, result);
@@ -78,43 +86,96 @@ fn main() {
             println!(
                 "Min result is now {}, elapsed seeds: {}, elapsed time: {:?}",
                 min_result,
-                i,
+                index,
                 start.elapsed()
             )
         }
-        // results.push(result);
-
-        // if results.len() % 10_000_000 == 0 {
-        //     println!(
-        //         "Running slow_function() took {} seconds and now has length {}.",
-        //         now.elapsed().as_secs(),
-        //         results.len()
-        //     );
-        //     now = Instant::now();
-        // }
+        index += 1;
     }
 
     // results.sort_by(|a, b| a.cmp(b));
-    // println!("lowest result is {}", results[0]);
+    println!("lowest result is {}", min_result);
 }
 
-fn parse_seeds(line: String) -> Vec<i64> {
-    let mut result: Vec<i64> = Vec::new();
-    let parts: Vec<i64> = line
-        .split(" ")
-        .map(|x| x.trim().parse::<i64>().unwrap())
-        .collect();
-    for i in (0..parts.len()).step_by(2) {
-        let start_range = parts[i];
-        let range_len = parts[i + 1];
-        for x in start_range..start_range + range_len {
-            result.push(x)
+struct Seeds {
+    parts: Vec<i64>,
+    index: usize,
+    current_range: Option<SeedsRange>,
+}
+
+#[derive(Debug)]
+struct SeedsRange {
+    index: i64,
+    start: i64, // inclusive
+    end: i64,   // exclusive
+}
+
+impl Iterator for SeedsRange {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // println!("calling seedsRange.next(), its index is {}", self.index);
+        let result = self.start + self.index;
+        // println!("result is {}", result);
+        if result >= self.end {
+            return None;
+        }
+
+        self.index += 1;
+        Some(result)
+    }
+}
+
+impl Seeds {
+    fn new(line: String) -> Seeds {
+        let parts: Vec<i64> = line
+            .split(" ")
+            .map(|x| x.trim().parse::<i64>().unwrap())
+            .collect();
+
+        // println!("Creating seeds from parts: {:?}", parts);
+        Seeds {
+            parts,
+            index: 0,
+            current_range: None,
         }
     }
-
-    result
 }
+impl Iterator for Seeds {
+    type Item = i64;
 
+    fn next(&mut self) -> Option<Self::Item> {
+        // println!("seeds.next() called, index is {}", self.index);
+
+        if self.current_range.is_some() {
+            let result = self.current_range.as_mut().unwrap().next();
+            if result.is_some() {
+                // println!("returning result of current range: {:?}", result);
+                return result;
+            }
+
+            // end of range, move on to the next one
+            // println!("end of range, move on to the next one");
+            self.current_range = None;
+            self.index += 2;
+        }
+
+        // end of all the seeds
+        if self.index >= self.parts.len() - 1 {
+            // println!("end of all seeds ranges");
+            return None;
+        }
+
+        self.current_range = Some(SeedsRange {
+            index: 0,
+            start: self.parts[self.index],
+            end: self.parts[self.index] + self.parts[self.index + 1],
+        });
+        // println!("created new current range: {:?}", self.current_range);
+
+        self.current_range.as_mut().unwrap().next()
+    }
+}
 fn create_computation(dest_range_start: i64, source_range_start: i64, range: i64) -> Computation {
     Computation {
         dest_range_start,
@@ -124,7 +185,7 @@ fn create_computation(dest_range_start: i64, source_range_start: i64, range: i64
 }
 
 struct Filter {
-    name: String,
+    _name: String,
     computations: Vec<Computation>,
 }
 
