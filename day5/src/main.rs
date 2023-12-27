daf0ee5e3d5e10a5ece18e1a3cbcb9ab1ec6fa54
Geert Gerritsen <444568+ggerritsen@users.noreply.github.com>
2023-12-27 15:30:37 +0100

    Day 5 part 2 WIP

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,7 @@
+use std::time::Instant;
+
 fn main() {
-    let input = include_str!("test_input.txt");
+    let input = include_str!("input.txt");
 
     let mut seeds: Vec<i64> = Vec::new();
     let mut filters: Vec<Filter> = Vec::new();
@@ -7,13 +9,18 @@ fn main() {
     for line in input.lines() {
         // process first line
         if line.starts_with("seeds: ") {
-            seeds = line
-                .replace("seeds: ", "")
-                .split(" ")
-                .map(|x| x.trim())
-                .filter(|x| !x.is_empty())
-                .map(|x| x.parse::<i64>().unwrap())
-                .collect();
+            // part 1
+            // seeds = line
+            //     .replace("seeds: ", "")
+            //     .split(" ")
+            //     .map(|x| x.trim())
+            //     .filter(|x| !x.is_empty())
+            //     .map(|x| x.parse::<i64>().unwrap())
+            //     .collect();
+
+            // part 2
+            seeds = parse_seeds(line.replace("seeds: ", ""));
+
             continue;
         }
 
@@ -51,22 +58,61 @@ fn main() {
         }
     }
 
-    println!("seeds is {:?}", seeds);
+    // println!("seeds is {:?}", seeds);
+    println!("len seeds is {:?}", seeds.len());
     println!("len filters is {:?}", filters.len());
 
-    let mut results: Vec<i64> = Vec::new();
-    for s in seeds {
-        let mut result = s;
+    // let mut results: Vec<i64> = Vec::new();
+    let mut min_result = i64::MAX;
+    let mut now = Instant::now();
+    let start = Instant::now();
+    for (i, s) in seeds.iter().enumerate() {
+        let mut result = *s;
         for f in &filters {
             result = f.apply(result);
-            println!("filter {} applied, result is now {}", f.name, result);
+            // println!("filter {} applied, result is now {}", f.name, result);
         }
-        println!("result is {result}");
-        results.push(result);
+        // println!("result is {result}");
+        if result < min_result {
+            min_result = result;
+            println!(
+                "Min result is now {}, elapsed seeds: {}, elapsed time: {:?}",
+                min_result,
+                i,
+                start.elapsed()
+            )
+        }
+        // results.push(result);
+
+        // if results.len() % 10_000_000 == 0 {
+        //     println!(
+        //         "Running slow_function() took {} seconds and now has length {}.",
+        //         now.elapsed().as_secs(),
+        //         results.len()
+        //     );
+        //     now = Instant::now();
+        // }
     }
 
-    results.sort_by(|a, b| a.cmp(b));
-    println!("lowest result is {}", results[0]);
+    // results.sort_by(|a, b| a.cmp(b));
+    // println!("lowest result is {}", results[0]);
+}
+
+fn parse_seeds(line: String) -> Vec<i64> {
+    let mut result: Vec<i64> = Vec::new();
+    let parts: Vec<i64> = line
+        .split(" ")
+        .map(|x| x.trim().parse::<i64>().unwrap())
+        .collect();
+    for i in (0..parts.len()).step_by(2) {
+        let start_range = parts[i];
+        let range_len = parts[i + 1];
+        for x in start_range..start_range + range_len {
+            result.push(x)
+        }
+    }
+
+    result
 }
 
 fn create_computation(dest_range_start: i64, source_range_start: i64, range: i64) -> Computation {
